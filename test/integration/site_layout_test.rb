204104fffd3405f2f8c5b26d052d14b1b516b40e
namelos <namelos@outlook.com>
2016-03-15 22:22:21 +0800

    add integration

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test 'layout links' do
+    get root_path
+  end
+end
